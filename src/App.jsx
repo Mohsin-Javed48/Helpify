c2b37dccabeb9aefb30d3f461245fe5187d38c57
Fakhra Rabbani <fakhrarabbani25@gmail.com>
2025-01-29 12:29:58 +0500

    comment added //hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 /** @format */
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "./ui/LoginPage";
-import SignupPage from "./ui/SignupPage";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/services/PlumberServices";
-import Blog from "./components/Blog";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import OrderPage from "./components/OrderPage";
-import AppLayout from "./components/AppLayout";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import PlumberServices from "./components/services/PlumberServices";
-import HandymanServices from "./components/services/HandymanServices";
-import AcRepairServices from "./components/services/AcRepairServices";
-import PainterServices from "./components/services/PainterServices";
-import CarpenterServices from "./components/services/CarpenterServices";
-import ElectricianServices from "./components/services/ElectricianServices";
-import HomeAppliencesServices from "./components/services/HomeAppliencesServices";
-import GeyserServices from "./components/services/GeyserServices";
-import GardnerServices from "./components/services/GardnerServices";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './ui/LoginPage';
+import SignupPage from './ui/SignupPage';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/services/PlumberServices';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import OrderPage from './components/OrderPage';
+import AppLayout from './components/AppLayout';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import PlumberServices from './components/services/PlumberServices';
+import HandymanServices from './components/services/HandymanServices';
+import AcRepairServices from './components/services/AcRepairServices';
+import PainterServices from './components/services/PainterServices';
+import CarpenterServices from './components/services/CarpenterServices';
+import ElectricianServices from './components/services/ElectricianServices';
+import HomeAppliencesServices from './components/services/HomeAppliencesServices';
+import GeyserServices from './components/services/GeyserServices';
+import GardnerServices from './components/services/GardnerServices';
 
 function App() {
   return (
@@ -68,5 +68,5 @@ function App() {
     </>
   );
 }
-
+//hello kia kro?
 export default App;

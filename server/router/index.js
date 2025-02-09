13995a550d0718087cd896e898f98ca960eaaf7e
zohaib <zuhaib.pucit@gmail.com>
2025-02-09 16:23:25 +0500

    services added to database

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const authRouter = require("./auth");
 const providerRouter = require("./provider");
+const serviceRouter = require("./service");
 const home = require("../controller/home");
 
 router.use("/auth", authRouter);
 router.use("/provider", providerRouter);
+router.use("/service", serviceRouter);
 
 router.get("/ping", home);
 

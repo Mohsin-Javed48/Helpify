e97025fca982e3aad4507558f7256d471fec0e1d
zohaib <zuhaib.pucit@gmail.com>
2025-02-09 12:35:47 +0500

    now only service provider can add services

--- server/router/provider.js
+++ server/router/provider.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 
 const { registerProvider, addService } = require("../controller/provider");
 const { authenticate } = require("../middlewares/authenticate");
+const { serviceProviderOnly } = require("../middlewares/serviceProviderOnly");
 
 router.post("/register", registerProvider);
-router.post("/add-service",authenticate, addService);
+router.post("/add-service",authenticate,serviceProviderOnly, addService);
 
 module.exports = router;

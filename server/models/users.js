fb4dccb3f01e3c317ca8e846dae2a9877a5a06a7
zohaib <zuhaib.pucit@gmail.com>
2025-02-09 10:09:44 +0500

    extra colum removed from user table

--- server/models/users.js
+++ server/models/users.js
@@ -24,11 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     address :{
       type: DataTypes.STRING,
       allowNull: true,
-    },
-    vatNumber :{
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    },    
     contact :{
       type: DataTypes.STRING,
       allowNull: true,
@@ -37,11 +33,7 @@ module.exports = (sequelize, DataTypes) => {
       type:DataTypes.STRING,
       allowNull:false
     },
-    role: {
-      type:DataTypes.INTEGER,
-      allowNull:false,
-      defaultValue:role.user,
-    },
+   
     forgetToken: {
        type: DataTypes.STRING,
        allowNull: true,
